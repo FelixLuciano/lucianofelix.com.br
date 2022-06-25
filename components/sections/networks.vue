9cd3147b30383257d828f9db825a892ba48d08bc
FelixLuciano <felix.lucianodias@gmail.com>
2022-06-25 02:18:50 -0300

    Networks section

---
+++ components/sections/networks.vue
@@ -0,0 +1,48 @@
+<template>
+  <page-section id="networks">
+    <template #title>
+      Connect with me
+    </template>
+
+    <ul class="network-list">
+      <ContentQuery path="/data/networks" :only="['networks']" find="one" #="{ data }">
+        <ContentRenderer :value="data" #="{ value:{ networks } }">
+          <li v-for="network in networks" :key="network.name" class="network-list__item">
+            <div class="network-list__item__title">{{ network.name }}</div>
+            <a :href="network.url" target="_blank" rel="noopener noreferer" class="network-list__link">{{ network.username }}</a>
+          </li>
+        </ContentRenderer>
+      </ContentQuery>
+    </ul>
+  </page-section>
+</template>
+
+<style lang="postcss" scoped>
+.network-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2rem;
+
+  &__item {
+    flex: 0 1 10rem;
+    list-style-type: square;
+
+    &::marker {
+      color: #666;
+    }
+
+    &__title {
+      font-size: .85rem;
+      font-weight: 600;
+      line-height: 1em;
+    }
+  }
+
+  &__link {
+    padding: .5rem 0;
+    border-bottom: 1px dotted #000;
+    font-weight: 500;
+    display: block;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,97 +2,14 @@
   <page-header />
 
   <main class="main__container">
-    <page-section id="about-me">
-      <template #title>
-        A student starting my career in computer engineering
-      </template>
-
-      <div class="photo-text-section">
-        <img src="assets/image/profile-photo.webp"/>
-
-        <ContentDoc path="/letters/about-me" class="prose" />
-      </div>
-    </page-section>
-
-    <page-section id="graduation">
-      <template #title>
-        Graduation 🎓
-      </template>
-
-      <div class="photo-text-section">
-        <div />
-
-        <ContentDoc path="/letters/graduation" class="prose" />
-      </div>
-    </page-section>
-
-    <page-section id="programming">
-      <template #title>
-        Programming 💻
-      </template>
-
-      <template #bottom>
-        <div class="gallery-grid">
-          <img src="assets/image/code-thumb.webp" v-for="i in 9" :key="i" :class="{'gallery-grid__item--feature': i % 5 === 1}" class="gallery-grid__item" />
-        </div>
-      </template>
-    </page-section>
-
-    <page-section id="photography">
-      <template #title>
-        Photography 📸
-      </template>
-
-      <template #bottom>
-        <div class="gallery-grid">
-          <img src="assets/image/photo-thumb.webp" v-for="i in 9" :key="i" :class="{'gallery-grid__item--feature': i % 5 === 1}" class="gallery-grid__item" />
-        </div>
-      </template>
-    </page-section>
-
-    <page-section id="photography">
-      <template #title>
-        Get in touch 📫
-      </template>
-
-      <template #header>
-        Send me a message
-      </template>
-
-      <div class="photo-text-section">
-        <div />
-
-        <form-frame url="#">
-          <template #="{ body, disabled }">
-            <form-text-input
-              name="contact"
-              :required="true"
-              :disabled="disabled"
-              v-model="body.contact"
-            >
-              <template #label>Contact</template>
-            </form-text-input>
-
-            <form-textarea-input
-              name="message"
-              :required="true"
-              :disabled="disabled"
-              v-model="body.message"
-            >
-              <template #label>Message</template>
-            </form-textarea-input>
-          </template>
-        </form-frame>
-      </div>
-    </page-section>
+    <sections-networks />
+    <sections-about-me />
+    <sections-graduation />
+    <sections-programming />
+    <sections-photography />
+    <sections-contact />
   </main>
 </template>
-
-<script lang="ts" setup>
-function log(a, b, c) {
-  console.log(a, b, c)
-}
-</script>
 
 
 <style lang="postcss">
@@ -104,16 +21,6 @@ function log(a, b, c) {
   flex-direction: column;
   gap: calc(1.5 * var(--margin-size));
   counter-reset: dec uni;
-}
-
-.photo-text-section {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 1rem;
-
-  @media (max-width: 64rem) {
-    grid-template-columns: 1fr;
-  }
 }
 
 .gallery-grid {
