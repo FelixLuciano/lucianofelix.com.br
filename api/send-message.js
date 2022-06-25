d3cc85b6591e7bd6204a0edb5098ef321d975a37
FelixLuciano <felix.lucianodias@gmail.com>
2022-06-25 14:34:23 -0300

    Added hCaptcha & Nodemailer

--- api/send-message.js
+++ api/send-message.js
@@ -1,10 +1,50 @@
-export default function handler(request, response) {
-  const { username, password } = process.env
+import { verify } from 'hcaptcha'
+import { createTransport } from 'nodemailer'
 
-  response.status(200).json({
-    success: true,
-    message: 'Hello world!',
-    username,
-    password
-  })
+
+const { secret, username, password, inbox } = process.env
+
+const mailer = createTransport(
+  {
+    service: "gmail",
+    auth: {
+      username,
+      password
+    }
+  },{
+    from: `LucianoFelix.com.br <${username}>`,
+    to: inbox,
+    subject: "New message",
+  }
+)
+
+export default function handler(request, response) {
+  try {
+    const { contact, message, token } = request.body
+    const { success } = await verify(secret, token)
+
+    if (success) {
+      const mail = {
+        text: `Conact: ${contact}\nMessage:\n${message}`
+      }
+
+      await mailer.sendMail(mail)
+
+      response.status(200).json({
+        success: true,
+        message: 'Message sent successfully! I will reply as soon as possible.'
+      })
+    } else {
+      response.status(400).json({
+        success: true,
+        message: 'Invalid captcha!'
+      })
+    }
+  }
+  catch {
+    response.status(500).json({
+      success: false,
+      message: 'Something went wrong :('
+    })
+  }
 }
