70c2e79e9d640dfd667611e8dd445bec04338b21
FelixLuciano <felix.lucianodias@gmail.com>
2022-06-25 17:54:40 -0300

    Send message via Google Sheets API

--- api/send-message.ts
+++ api/send-message.ts
@@ -1,50 +1,66 @@
+import { google } from 'googleapis'
 import { verify } from 'hcaptcha'
-import { createTransport } from 'nodemailer'
 
 
-const { secret, username, password, inbox } = process.env
+const sheets = google.sheets('v4')
+const sheetId = '1oWWT-Fw0FQJdGO3VbtDFh3x_Uvid7bNuptGNWQkxZXM'
+const range = 'Messages'
+const { secret, google_credentials } = process.env
 
-const mailer = createTransport(
-  {
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-      username,
-      password
-    }
-  },{
-    from: `LucianoFelix.com.br <${username}>`,
-    to: inbox,
-    subject: "New message",
-  }
-)
+async function authenticate () {
+  const auth = new google.auth.GoogleAuth({
+    scopes: [
+      'https://www.googleapis.com/auth/drive',
+      'https://www.googleapis.com/auth/drive.file',
+      'https://www.googleapis.com/auth/spreadsheets'
+    ],
+    credentials: JSON.parse(google_credentials),
+  })
+  const authClient = await auth.getClient()
+
+  google.options({ auth: authClient })
+}
 
 export default async function handler(request, response) {
   try {
     const { contact, message, token } = request.body
     const { success } = await verify(secret, token)
 
-    if (success) {
-      const mail = {
-        text: `Conact: ${contact}\nMessage:\n${message}`
-      }
-
-      await mailer.sendMail(mail)
-
-      response.status(200).json({
-        success: true,
-        message: 'Message sent successfully! I will reply as soon as possible.'
-      })
-    } else {
-      response.status(400).json({
-        success: true,
+    if (!success) {
+      response.code(400).json({
+        success: false,
         message: 'Invalid captcha!'
+      })
+    }
+    else {
+      await authenticate()
+
+      await sheets.spreadsheets.values.append({
+        spreadsheetId: sheetId,
+        range: range,
+        valueInputOption: 'RAW',
+        insertDataOption: 'INSERT_ROWS',
+        requestBody: {
+          range: range,
+          majorDimension: 'ROWS',
+          values: [
+            [
+              new Date().toLocaleString('pt-BR'),
+              contact,
+              message
+            ]
+          ]
+        }
+      })
+
+      response.code(200).json({
+        success: true,
+        message: 'Success!'
       })
     }
   }
   catch {
-    response.status(500).json({
+    response.code(500).json({
       success: false,
       message: 'Something went wrong :('
     })
