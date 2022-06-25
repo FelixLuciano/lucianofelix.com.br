8db87d6c89b35cbafe0e1bc255e8257a0891060f
FelixLuciano <felix.lucianodias@gmail.com>
2022-06-24 21:36:33 -0300

    Fix section counter wraping

--- components/page/section.vue
+++ components/page/section.vue
@@ -1,27 +1,29 @@
 <template>
   <section class="page-section" :id="`section__${id}`">
+    <div class="page-section__divider" />
+
     <div v-if="$slots.top" class="page-section__slot page-section__slot--top">
       <slot name="top" />
     </div>
 
-    <div class="page-section__slot page-section__slot--left">
-      <div class="page-section__counter" />
-    </div>
+    <div class="page-section__wrap">
+      <div class="page-section__slot page-section__slot--left">
+        <div class="page-section__counter" />
+      </div>
 
-    <div class="page-section__container">
-      <header class="page-section__header">
-        <h2 class="page-section__header__title">
-          <a :href="`#section__${id}`"><slot name="title" /></a>
-        </h2>
+      <div class="page-section__container">
+        <header class="page-section__header">
+          <h2 class="page-section__header__title">
+            <a :href="`#section__${id}`"><slot name="title" /></a>
+          </h2>
 
-        <slot name="header" v-if="$slots.header" />
-      </header>
+          <slot name="header" v-if="$slots.header" />
+        </header>
 
-      <slot/>
-    </div>
+        <slot/>
+      </div>
 
-    <div class="page-section__slot page-section__slot--right">
-
+      <div class="page-section__slot page-section__slot--right"></div>
     </div>
 
     <div v-if="$slots.bottom" class="page-section__slot page-section__slot--bottom">
@@ -47,7 +49,7 @@ defineProps({
   margin: 0 auto;
   padding: 1rem 0;
   display: flex;
-  flex-wrap: wrap;
+  flex-direction: column;
   gap: 1rem;
   counter-increment: uni;
 
@@ -59,8 +61,15 @@ defineProps({
     counter-set: uni 0;
   }
 
+  &__divider {
+    width: 100%;
+    max-width: 80rem;
+    margin: 0 auto 1rem auto;
+    border-top: 1px dotted #777;
+  }
+
   &__slot {
-    flex: 1 0 0;
+    flex: 1 1 0;
     display: flex;
 
     &--top, &--bottom {
@@ -68,9 +77,14 @@ defineProps({
     }
   }
 
+  &__wrap {
+    display: flex;
+    flex-wrap: nowrap;
+  }
+
   &__counter {
     width: 100%;
-    padding-top: 2.75rem;
+    padding-top: .75rem;
     padding-right: calc(var(--margin-size) / 2);
     color: #666;
     font-size: 1rem;
@@ -88,15 +102,13 @@ defineProps({
   &__container {
     flex: 1 1 auto;
     margin: 0 auto;
-    padding-top: 2rem;
-    border-top: 1px dotted #777;
     width: 100%;
     max-width: 80rem;
   }
 
   &__header {
     max-width: 40rem;
-    padding-bottom: 1rem;
+    padding-bottom: 1.5rem;
     font-size: 1.25rem;
     line-height: 1em;
 
