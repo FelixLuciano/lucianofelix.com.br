7564ff7052a704455acfb475f6784514d7cd965c
FelixLuciano <felix.lucianodias@gmail.com>
2022-10-06 23:07:19 -0300

    refactor(typescript): :wastebasket: remove deprecated import

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,7 @@
-import { defineNuxtConfig } from 'nuxt'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   target: 'static',
   ssr: false,
-  buildAssetsDir: '_app',
 
   modules: [
     // https://go.nuxtjs.dev/tailwindcss
