5a3e369172c2303380a635098dd131e192956f60
FelixLuciano <felix.lucianodias@gmail.com>
2022-12-08 16:08:53 -0300

    chore: remove logs

--- api/send-message.ts
+++ api/send-message.ts
@@ -26,8 +26,6 @@ export default async function handler(request, response) {
     const { contact, message, token } = request.body
     const { success } = await verify(secret, token)
 
-    console.log(secret, token, success)
-
     if (!success) {
       response.status(400).json({
         success: false,
