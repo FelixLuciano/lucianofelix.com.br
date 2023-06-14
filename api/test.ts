0a512b52b7a380762f749d53033676aab29cc9f3
FelixLuciano <felix.lucianodias@gmail.com>
2023-06-14 14:21:35 -0300

    fix: response content type

--- api/test.ts
+++ api/test.ts
@@ -3,11 +3,14 @@ export const runtime = 'edge'
 export async function GET(request: Request) {
   const body = JSON.stringify({
     success: true,
-    message: 'Hello World!'
+    message: 'Hello World!',
   })
 
   return new Response(body, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
     status: 200,
-    statusText: 'OK'
+    statusText: 'OK',
   })
 }
